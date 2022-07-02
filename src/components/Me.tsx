265b679d7d92255db350585f17c725b4a817eba7
corentin <thomas.corentin57@gmail.com>
2022-07-02 15:54:25 +0200

    feat(me): image loading eager + blur placeholder

--- src/components/Me.tsx
+++ src/components/Me.tsx
@@ -26,7 +26,7 @@ export function Me({setContactFormModalOpen}: MeProps) {
             <div className="relative mx-auto max-w-5xl pt-16 sm:px-6">
                 <div className="bg-slate-50 pt-px sm:rounded-6xl">
                     <div className="relative mx-auto -mt-16 h-44 w-44 overflow-hidden rounded-full bg-slate-200 md:float-right md:h-64 md:w-64 md:[shape-outside:circle(40%)] lg:mr-20 lg:h-72 lg:w-72">
-                        <Image src={logo} layout='fill' alt={'logo'} />
+                        <Image src={logo} loading={'eager'} placeholder={'blur'} layout={'fill'} alt={'logo'} />
                     </div>
                     <div className="px-4 py-10 sm:px-10 sm:py-16 md:py-20 lg:px-20 lg:py-32">
                         <h1 className="mt-16 font-display text-5xl font-extrabold tracking-tight text-slate-900 sm:text-6xl">
